9028f93ac7ca85fdc05b19a7d8909a91987ce7d6
mrding <mrding311@163.com>
2015-11-10 23:47:18 +0800

    修复最简工程

--- src/main/java/com/mrding/one/web/IndexController.java
+++ src/main/java/com/mrding/one/web/IndexController.java
@@ -1,6 +1,7 @@
 package com.mrding.one.web;
 
 import com.mrding.one.service.IndexService;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
